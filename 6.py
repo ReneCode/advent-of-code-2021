b4f6ea3c26cf6f520dd43de6c3ce41787c4c487b
Reinhard Langner <ReinhardLangner@gmx.de>
2022-04-18 16:07:39 +0200

    day 6 laterna fish

---
+++ 6.py
@@ -0,0 +1,49 @@
+# https://adventofcode.com/2021/day/6
+
+import util
+
+
+class Factory:
+  def __init__(self, start_timers):
+    self.fishes = []
+    for timer in start_timers:
+      self.create(timer)
+
+  def create(self, nr):
+    fish = Fish(nr)
+    self.fishes.append(fish)
+
+  def tick(self):
+    all_fishes = self.fishes.copy()
+    for fish in all_fishes:
+      fish.tick(self)
+
+  def print(self):
+    numbers = [str(f.timer) for f in self.fishes]
+    print(numbers)
+
+
+class Fish:
+  def __init__(self, nr):
+    self.timer = nr
+
+  def tick(self, factory):
+    if self.timer > 0:
+      self.timer = self.timer -1
+      return False
+    self.timer = 6
+    factory.create(8)
+    return True
+
+
+
+input_data = util.read_data('./6.data')
+start_timer = [int(n) for n in input_data[0].split(',')]
+factory = Factory(start_timer)
+factory.print()
+
+days = 80
+for i in range(days):
+  factory.tick()
+  # factory.print()
+print(len(factory.fishes))
