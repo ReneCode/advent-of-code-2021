7ed39a0c404dc893ff850a68819731100bd8f892
Reinhard Langner <ReinhardLangner@gmx.de>
2022-04-24 16:59:30 +0200

    complete day 9 part-2

--- 9.py
+++ 9.py
@@ -55,13 +55,61 @@ def get_values(board, coords):
     result.append(board[y][x])
   return result
 
-board = read_data()
-low_coords = get_low_points(board)
-result = get_values(board, low_coords)
-risk_level = [n+1 for n in result]
-result_sum = sum(risk_level)
+def get_neighbour_coords(board, coord):
+  xlen = len(board[0])
+  ylen = len(board)
+  (x,y) = coord
+  result = []
+  if x > 0:
+    result.append( (x-1,y) )
+  if x+1 < xlen:
+    result.append( (x+1,y) )
+  if y > 0:
+    result.append( (x, y-1) )
+  if y+1 < ylen:
+    result.append( (x, y+1) )
+  return result
 
-print(board)
-print(low_coords)
-print(result)
-print(result_sum)
+def get_value(board, coord):
+  (x,y) = coord
+  return board[y][x]
+
+def add_heat_area(board, coord, heat_area):
+  # do not add duplicate coords
+  if coord in heat_area:
+    return
+
+  heat_area.append(coord)
+  cur_val = get_value(board, coord)
+  neighboards_coords = get_neighbour_coords(board, coord)
+  for neigh_coord in neighboards_coords:
+    val = get_value(board, neigh_coord)
+    if val != MAX_NR and val > cur_val:
+      add_heat_area(board, neigh_coord, heat_area)
+
+
+
+def part_1():
+  board = read_data()
+  low_coords = get_low_points(board)
+  result = get_values(board, low_coords)
+  risk_level = [n+1 for n in result]
+  result_sum = sum(risk_level)
+
+  print(board)
+  print(low_coords)
+  print(result)
+  print(result_sum)
+
+board = read_data()
+coords = get_low_points(board)
+heat_sizes = []
+for coord in coords:
+  heat_area = []
+  add_heat_area(board, coord, heat_area)
+  heat_sizes.append(len(heat_area))
+  print(f'{coord} / {heat_area} / {len(heat_area)}')
+
+heat_sizes = sorted(heat_sizes, reverse=True)
+result = heat_sizes[0] * heat_sizes[1] * heat_sizes[2]
+print(f'Result: {result}')
