f90ef003c828548e27ab0587d7e03e697ac8d3d3
Reinhard Langner <ReinhardLangner@gmx.de>
2022-06-14 22:01:34 +0200

    day 19 part-1 solved

--- 19.py
+++ 19.py
@@ -2,11 +2,18 @@
 # https://adventofcode.com/2021/day/19
 
 import util
+import vector
 
-# return p1 - p2
-def pos_sub(p1, p2):
-  return (p1[0]-p2[0], p1[1]-p2[1], p1[2]-p2[2])
 
+
+def calc_deltas(positions, idx_pt):
+  deltas = []
+  rel_pt = positions[idx_pt]
+  for i in range(len(positions)):
+    if i != idx_pt:
+      delta = vector.sub(positions[i], rel_pt)
+      deltas.append(delta)
+  return deltas
 
 class Scanner:
   def __repr__(self):
@@ -15,15 +22,17 @@ class Scanner:
   def __init__(self, nr, positions):
     self.nr = nr
     self.positions = positions
+    self.mapping = None
+    self.offset = None
 
-  def calc_deltas(self, idx_pt):
-    deltas = []
-    rel_pt = self.positions[idx_pt]
-    for i in range(len(self.positions)):
-      if i != idx_pt:
-        delta = pos_sub(self.positions[i], rel_pt)
-        deltas.append(delta)
-    return deltas
+  def set_mapping(self, mapping):
+    self.mapping = mapping
+
+  def set_offset(self, vec):
+    self.offset = vec
+
+  def get_global_positions(self):
+    return [vector.add(self.offset, map_vec(vec, self.mapping)) for vec in self.positions]
 
 def read_data(filename):
   lines = util.read_data(filename)
@@ -77,6 +86,9 @@ def map_vec(vec, mapping):
   z_sgn = sign(mapping[2])
   return (vec[x_idx]*x_sgn, vec[y_idx]*y_sgn, vec[z_idx]*z_sgn)
 
+def invers_map_vec(vec, mapping):
+  (x,y,z) = map_vec(vec, mapping)
+  return (-x,-y,-z)
 
 def get_all_mappings():
   bases = [(1,2,3), (1,3,2), (2,1,3), (2,3,1), (3,1,2), (3,2,1)]
@@ -95,29 +107,74 @@ def get_all_mappings():
 
 
 def find_similar_pos(scanner_a, scanner_b, all_mappings):
-
-
   for idx_a in range(len(scanner_a.positions)):
-    deltas_a = scanner_a.calc_deltas(idx_a)
+    mapped_positions_a = [map_vec(vec, scanner_a.mapping) for vec in scanner_a.positions]
+    deltas_a = calc_deltas(mapped_positions_a, idx_a)
     for idx_b in range(len(scanner_b.positions)):
-      deltas_b = scanner_b.calc_deltas(idx_b)
+      if scanner_b.mapping != None:
+        all_mappings = [scanner_b.mapping]
       for mapping in all_mappings:
-        mapped_deltas_a = [map_vec(vec, mapping) for vec in deltas_a]
-        found = is_good_fit(mapped_deltas_a, deltas_b)
+        mapped_positions_b = [map_vec(vec, mapping) for vec in scanner_b.positions]
+        deltas_b = calc_deltas(mapped_positions_b, idx_b)
+        found = is_good_fit(deltas_a, deltas_b)
         if found:
-          vec_a = scanner_a.positions[idx_a]
-          mapped_vec_a = map_vec(vec_a, mapping)
-          vec_b = scanner_b.positions[idx_b]
-          relative = pos_sub(vec_b, mapped_vec_a)
-          print(f'found {idx_a} {vec_a}, {idx_b} {vec_b} mapping:{mapping} relative:{relative}')
-          break
+          # with that mapping b-positions are mapped into a-system
+          scanner_b.set_mapping(mapping)
+          vec_a = mapped_positions_a[idx_a]
+          vec_b = mapped_positions_b[idx_b]
+          relative = vector.sub(vec_a, vec_b)
+          b_offset = vector.add(scanner_a.offset, relative)
+          scanner_b.set_offset(b_offset)
+          # print(f'found {idx_a} {vec_a}, {idx_b} {vec_b} mapping:{mapping} relative:{relative} {mapped_relative}')
+          return True
+  return False
 
 
-scanners = read_data('./19-example.data')
-s1 = scanners[0]
-s2 = scanners[1]
-all_mappings = get_all_mappings()
-find_similar_pos(s1, s2, all_mappings)
 
 
- 
+def check_overlap(idx):
+  all_mappings = get_all_mappings()
+  sa = scanners[idx]
+  for i in range(len(scanners)):
+    if i != idx:
+      sb = scanners[i]
+      if sb.offset == None:
+        found = find_similar_pos(sa, sb, all_mappings)
+        if found:
+          print(idx, i, found)
+          check_overlap(i)
+
+
+
+
+scanners = read_data('./19.data')
+scanners[0].set_offset((0,0,0))
+scanners[0].set_mapping((1,2,3))
+check_overlap(0)
+for scanner in scanners:
+  print(scanner.offset, scanner.mapping)
+
+all_points = {}
+for scanner in scanners:
+  positions = scanner.get_global_positions()
+  for pos in positions:
+    all_points[pos] = True
+
+# print(all_points)
+print(f'count all points: {len(all_points)}')
+
+# print(points2)
+
+ # scanner 1    68, -1246,  -43 
+ # scanner 2  1105, -1205, 1229 
+ # scanner 3   -92, -2380,  -20
+ # scanner 4   -20, -1133, 1061
+
+
+relative = """
+ 0:  (  0,     0,     0) ( 1,  2,  3)
+0-1: ( 68, -1246,   -43) (-1,  2, -3)
+4-2: (168, -1125,    72) ( 2,  1, -3)
+1-3: (160, -1134,   -23) ( 1,  2,  3)
+1-4: ( 88,   113, -1104) ( 2, -3, -1)
+"""
---
+++ vector.py
@@ -0,0 +1,38 @@
+
+# vector
+# (x,y,z)
+
+# return p1 - p2
+def sub(p1, p2):
+  return (p1[0]-p2[0], p1[1]-p2[1], p1[2]-p2[2])
+
+# return p1 + p2
+def add(p1, p2):
+  return (p1[0]+p2[0], p1[1]+p2[1], p1[2]+p2[2])
+
+
+class Matrix:
+  #
+  # (a, d, g, j)
+  # (b, e, h, k)
+  # (c, f, i, l)
+  # (0, 0, 0, 1)
+  def __init__(self,a,b,c,d,e,f,g,h,i,j,k,l):
+    self.a = a
+    self.b = b
+    self.c = c
+    self.d = d
+    self.e = e
+    self.f = f
+    self.g = g
+    self.h = h
+    self.i = i
+    self.j = j
+    self.k = k
+    self.l = l
+
+  def translate(dx,dy,dz):
+    return Matrix(0,0,0,0,0,0,0,0,0,dx,dy,dz)
+
+  def multiply(max):
+    pass
