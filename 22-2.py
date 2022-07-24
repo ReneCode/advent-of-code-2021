93165342ce3155e32fa0de0a80247410a1d92d01
Reinhard Langner <ReinhardLangner@gmx.de>
2022-07-24 11:26:46 +0200

    testing for 22

---
+++ 22-2.py
@@ -0,0 +1,195 @@
+
+# https://adventofcode.com/2021/day/22
+
+import util
+
+COORD_X = 0
+COORD_Y = 1
+COORD_Z = 2
+
+def inside(rg, x):
+  return rg[0] <= x and x <= rg[1]
+
+def left_from(rg, x):
+  return x < rg[0]
+
+def right_from(rg, x):
+  return rg[1] < x
+
+
+"""
+cut_out
+
+a:    456789
+b: 123
+=> None
+
+a:    456789
+b: 1234
+> cut:4
+ rest: 56789
+
+a:    456789
+b: 12345678
+> cut:45678
+ rest:     9
+
+a:    456789
+b: 123456789
+> cut:456789
+
+a:    456789
+b: 1234567890
+> cut:456789
+
+a:    456789
+b:    4567
+> cut:4567
+ rest:    89
+
+a:    456789
+b:     567
+> cut: 567
+ rest:4   89
+
+a:    456789
+b:      67890
+> cut:  6789
+ rest:45  
+
+a:    456789
+b:         90
+> cut:     9
+ rest:45678
+
+a:    456789
+b:          0
+> None
+
+"""
+
+# rg_a = (start_nr, stop_nr)
+# rg_b = (start_nr, stop_nr)
+def get_left_cut_right(rg_a, rg_b):
+  if rg_b[1] < rg_a[0]:
+    return None
+  if rg_a[1] < rg_b[0]:
+    return None
+  left = None
+  cut = None
+  right = None
+  if rg_a[0] < rg_b[0]:
+    left = (rg_a[0], rg_b[0]-1)
+  if rg_b[1] < rg_a[1]:
+    right = (rg_b[1]+1, rg_a[1])
+  cut = (max(rg_a[0], rg_b[0]), min(rg_a[1], rg_b[1]))
+  return (left, cut, right)
+
+class Box:
+  def __init__(self, x, y, z):
+    self.x = x
+    self.y = y
+    self.z = z
+
+  def __repr__(self):
+    xr = f'x:{self.x[0]}-{self.x[1]}'
+    yr = f'y:{self.y[0]}-{self.y[1]}'
+    zr = f'z:{self.z[0]}-{self.z[1]}'
+    return f'{xr} {yr} {zr}'
+
+  def __eq__(self, o):
+    if isinstance(self, o.__class__):
+      return self.x == o.x and self.y == o.y and self.z == o.z
+    return False
+
+  def intercept(self, other):
+    # x
+    if self.x[0] > other.x[1]:
+      return False  # self is right of other
+    if self.x[1] < other.x[0]:
+      return False  # self is left of other
+    # y
+    if self.y[0] > other.y[1]:
+      return False  # self is right of other
+    if self.y[1] < other.y[0]:
+      return False  # self is left of other
+    # z
+    if self.z[0] > other.z[1]:
+      return False  # self is right of other
+    if self.z[1] < other.z[0]:
+      return False  # self is left of other
+    return True
+
+  def cutout(self, cut_box):
+    rest_boxes = []
+    box = self
+    result_x = get_left_cut_right(box.x, cut_box.x)
+    if result_x != None:
+      if result_x[0] != None:
+        rest_boxes.append(Box(result_x[0], box.y, box.z))
+      if result_x[2] != None:
+        rest_boxes.append(Box(result_x[2], box.y, box.z))
+      # this is the cutout
+      box = Box(result_x[1], box.y, box.z)
+
+    result_y = get_left_cut_right(box.y, cut_box.y)
+    if result_y != None:
+      if result_y[0] != None:
+        rest_boxes.append(Box(box.x, result_y[0], box.z))
+      if result_y[2] != None:
+        rest_boxes.append(Box(box.x, result_y[2], box.z))
+      # this is the cutout
+      box = Box(box.x, result_y[1], box.z)
+
+    result_z = get_left_cut_right(box.z, cut_box.z)
+    if result_z != None:
+      if result_z[0] != None:
+        rest_boxes.append(Box(box.x, box.y, result_z[0]))
+      if result_z[2] != None:
+        rest_boxes.append(Box(box.x, box.y, result_z[2]))
+      # this is the cutout
+      box = Box(box.x, box.y, result_z[1])
+
+    return rest_boxes
+
+
+def read_range(str):
+  tok = str.split("=")
+  tok = tok[1].split("..")
+  return (int(tok[0]), int(tok[1]))
+
+def read_data(filename):      
+  lines = util.read_data(filename)
+  result = []
+  for line in lines:
+    tok = line.split(" ")
+    on = tok[0] == 'on'
+    coord = tok[1].split(',')
+    x = read_range(coord[0])
+    y = read_range(coord[1])
+    z = read_range(coord[2])
+    box = Box(x,y,z)
+    result.append( (on, box))
+  return result
+
+
+
+
+def add_box(box_a, box_b):
+  result = [box_a]
+#   for box in result:
+
+
+
+#   for box in boxes:
+#     result.extend( box.split('x', add_box.x[0]) )
+#     result.extend( box.split('x', add_box.x[1]) )
+
+data = read_data('./22.data')
+boxes = []
+for d in data:
+  on = d[0]
+  box = d[1]
+    
+
+
---
+++ test_22.py
@@ -0,0 +1,43 @@
+
+t = __import__('22-2')
+
+def test_insert_more_outside():
+  result = t.get_left_cut_right((2,5), (6,8))
+  assert result == None
+
+def test_insert():
+  result = t.get_left_cut_right((2,5), (3,4))
+  assert result == ((2,2), (3,4),(5,5))
+  
+def test_insert_more_left():
+  result = t.get_left_cut_right((2,5), (1,4))
+  assert result == (None, (2,4),(5,5))
+
+def test_insert_more_rightborder():
+  result = t.get_left_cut_right((2,5), (5,7))
+  assert result == ((2,4), (5,5),None)
+
+
+def test_cutout_1():
+  box_a = t.Box((10,30), (20,40), (30,50))
+  box_b = t.Box((1,5), (20,25), (60,70))
+  result = box_a.cutout(box_b)
+  expect = [
+    t.Box((10,30), (26,40), (30,50))
+  ]
+  assert result == expect
+
+def test_cutout_2():
+  box_a = t.Box((10,30), (20,40), (30,50))
+  box_b = t.Box((15,20), (20,25), (60,70))
+  result = box_a.cutout(box_b)
+  expect = [
+    t.Box((10,14), (20,40), (30,50)),
+    t.Box((21,30), (20,40), (30,50)),
+    t.Box((15,20), (26,40), (30,50))
+  ]
+  assert result == expect
+
+
+
+test_cutout_1()
